09dba5c0df28a0d5ee990f6c707532868b088fa3
John-Progr <john314325@gmail.com>
2025-10-31 19:48:49 +0200

    fixed singlehop

--- constants/things.py
+++ constants/things.py
@@ -5,6 +5,6 @@ class ThingsConstants:
     # Dictionary mapping IP addresses to thing IDs
     IP_TO_THING_ID = {
         "192.168.2.20": "device2",
-        "192.168.2.30": "device1",
+        "192.168.2.10": "device1",
         "192.168.2.40": "device4",
     }
